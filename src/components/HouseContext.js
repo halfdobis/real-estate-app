520e4a7f56585db5a8f79a740782a80111dc4af1
missdobis <missdobis@gmail.com>
2023-10-01 23:42:41 +0100

    [Hero-Section, Filter-Section]

--- src/components/HouseContext.js
+++ src/components/HouseContext.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect, createContext } from "react";
 
-const HouseContext = () => {
-  return <div>HouseContext</div>;
+import { housesData } from "../data";
+
+export const HouseContext = createContext();
+
+const HouseContextProvider = ({ children }) => {
+  const [houses, setHouses] = useState(housesData);
+  const [country, setCountry] = useState("Location (any)");
+  return (
+    <HouseContext.Provider value={console.log("this is the context")}>
+      {children}
+    </HouseContext.Provider>
+  );
 };
 
-export default HouseContext;
+export default HouseContextProvider;
